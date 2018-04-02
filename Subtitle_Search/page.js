96b776eb3aa46a7b585df7883e43eea25ef280f8
Raunak Adak <raunak249@gmail.com>
2018-04-03 02:26:57 +0530

    Update page.js
    Made changes due to the addition of radio buttons in the corresponding html file.

--- Subtitle_Search/page.js
+++ Subtitle_Search/page.js
@@ -1,10 +1,22 @@
 
+
+/*document.getElementById("sbutton").addEventListener("keypress",function(event){
+  if (event.keyCode === 13) {
+	alert('HI');
+  }
+});
+*/
 document.getElementById("sbutton").addEventListener("click",doSomething);
-
-
 function doSomething(){
 	var x=document.getElementById('word').value;
-    var url="https://subscene.com/subtitles/title?q="+ x + "&l=";
+	if (document.getElementById('r1').checked) {
+		var url="https://subscene.com/subtitles/title?q="+ x + "&l=";
+	}
+	else if(document.getElementById('r2').checked){
+		var url="https://www.opensubtitles.org/en/search2/sublanguageid-all/moviename-"+ x;
+	}
+	else if(document.getElementById('r3').checked){
+		var url="https://www.yifysubtitles.com/search?q=" + x;
+	}
 	browser.tabs.create({url:url});
 }
-
